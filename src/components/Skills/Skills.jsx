68b67d6e171864e945f2962a8abf0c71009499a6
Wojciech Kubiak <wgkubiak@gmail.com>
2021-03-26 23:31:40 +0100

    Mobile / web switch for skills

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -13,9 +13,24 @@ import HTMLImg from "./../../images/htmlb.png";
 import CSSImg from "./../../images/cssb.png";
 
 const Skills = (props) => {
+  const [showDescription, setShowDescription] = useState(false);
+
   const { ref, inView, entry } = useInView({
     threshold: isMobile ? 0.1 : 0.4,
   });
+
+  let descriptionRef = useRef(null);
+
+  const descriptionHandler = (show) => {
+    console.log(show);
+    if(show) {
+      setShowDescription(true);
+    } else {
+      setShowDescription(false);
+
+    }
+
+  }
 
   const skillsList = [
     {
@@ -61,7 +76,6 @@ const Skills = (props) => {
   ];
 
   const Tile = (img, name, index) => {
-    const [showDescription, setShowDescription] = useState(false);
     const [animated, setAnimated] = useState(false);
 
     let contentRef = useRef(null);
@@ -74,26 +88,31 @@ const Skills = (props) => {
       if (inView && !animated && !isMobile) {
         const t1 = gsap.timeline();
 
-        t1.to(contentRef, { y: -30, opacity: 1, duration: 1, onComplete: () => setAnimated(true)}).delay(index * 0.2);
+        t1.to(contentRef, {
+          y: -30,
+          opacity: 1,
+          duration: 1,
+          onComplete: () => setAnimated(true),
+        }).delay(index * 0.2);
       }
     }, [inView]);
 
     return (
       <div
-        ref={r => contentRef = r}
+        ref={(r) => (contentRef = r)}
         key={`tile-${name}`}
-        onMouseOver={() => setShowDescription(true)}
-        onMouseOut={() => setShowDescription(false)}
+        onMouseOver={() => descriptionHandler(true)}
+        onMouseOut={() => descriptionHandler(false)}
       >
         <img src={img} alt={name} />
-        <h1>{name}</h1>
-        {showDescription && (
-          <div className="skills-tile-description">
-            <p>Description</p>
-            <p>Description</p>
-            <p>Description</p>
-          </div>
-        )}
+        <h1>{name}</h1> 
+        {isMobile && <div
+            className="skills-tile-description-mobile"
+          >
+            <p>Description dsa dsa dsa dsa </p>
+            <p>Description das  dsa</p>
+            <p>Descriptiond sa sa dsa dsa dsa dsa dsa</p>
+          </div>}
       </div>
     );
   };
@@ -101,11 +120,21 @@ const Skills = (props) => {
   return (
     <div className="skills-page" ref={ref}>
       <h1 className="skills-header">SKILLS</h1>
-      <div className="skills-tiles" >
+      <div className="skills-tiles">
         {skillsList.map((element, index) => {
           return Tile(element.img, element.name, index);
         })}
       </div>
+      {showDescription && !isMobile && (
+          <div
+            className="skills-tile-description"
+            ref={(r) => (descriptionRef = r)}
+          >
+            <p>Description dsa dsa dsa dsa </p>
+            <p>Description das  dsa</p>
+            <p>Descriptiond sa sa dsa dsa dsa dsa dsa</p>
+          </div>
+        )}
     </div>
   );
 };
