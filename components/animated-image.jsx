2bb3160f2b47391b599911eda451acdeff57401c
wojciechkubiak <wgkubiak@gmail.com>
2021-08-07 22:50:25 +0200

    Mobile animation pause + resize

--- components/animated-image.jsx
+++ components/animated-image.jsx
@@ -1,18 +1,25 @@
 import Lottie from "react-lottie";
+import { useInView } from "react-intersection-observer";
 
-const AnimatedImage = (props) => (
-  <div className="lottie-main">
-    <div className="lottie-container">
-      <Lottie
-        options={props.options}
-        height={props.height}
-        width={props.width}
-        isStopped={false}
-        isPaused={false}
-      />
+const AnimatedImage = (props) => {
+  const { ref, inView, entry } = useInView({
+    threshold: 0.3,
+  });
+
+  return (
+    <div className="lottie-main" ref={ref}>
+      <div className="lottie-container">
+        <Lottie
+          options={props.options}
+          height={props.height}
+          width={props.width}
+          isStopped={false}
+          isPaused={!inView}
+        />
+      </div>
+      <h2 className="landing-skills-name">{props.text}</h2>
     </div>
-    <h2 className="landing-skills-name">{props.text}</h2>
-  </div>
-);
+  );
+};
 
 export default AnimatedImage;
